b1185508a7d24af60bce4123b61431d66698687f
MayaCoro <mayascoro@gmail.com>
2018-06-06 23:00:42 +0000

    automatically backed up by learn

---
+++ amazon_cart.rb
@@ -0,0 +1,29 @@
+# cart_item_prices = [12.43, 19.89, 3.49, 75.00]
+# cart_item_prices.push(5.30)
+
+# puts cart_item_prices
+
+# count = 1
+# cart_item_prices.each do |prices| 
+#   puts "price is #{prices}"
+#   count += 1
+# end
+
+# cart_item_prices = [12.43, 19.89, 3.49, 75.00]
+# tax_included = []
+# cart_item_prices.each do |price|
+# price_with_tax = price * 1.08
+# tax_included << pices_with_tax
+# end
+# puts tax_included
+
+cart_item_prices = [12.43, 19.89, 3.49, 75.00]
+big_ticket_prices = []
+cart_item prices.each do |prices|
+  if prices >= 15
+    puts big_ticket_prices << prices
+  end
+end
+
+big_ticket_prices
+
---
+++ arrays.rb
@@ -0,0 +1,15 @@
+# student_1 = "Maya"
+# student_2 = "Maddie"
+# student_3 = "Katlyn"
+# student_4 = "Cher"
+
+student_names = ["Maya", "Maddie"]
+
+#puts student_names
+#puts student_names[0]
+student_names.push("Katlyn")
+student_names.push("Cher")
+student_names.push("Beyonce")
+
+#puts student_names.size 
+puts student_names[4]
